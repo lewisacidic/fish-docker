0f9f3998209aaa1905d20646f276a984df2ec735
Gazorby <gazorby@pm.me>
2020-11-04 22:01:21 +0100

    ♻️ refactor: use fisher events

--- conf.d/fish_docker_abbr.fish
+++ conf.d/fish_docker_abbr.fish
@@ -5,329 +5,209 @@
 
 # Adapted from https://github.com/akarzim/zsh-docker-aliases
 
-# Docker
-abbr dk 'docker'
-abbr dka 'docker attach'
-abbr dkb 'docker build'
-abbr dkd 'docker diff'
-abbr dkdf 'docker system df'
-abbr dke 'docker exec'
-abbr dkei 'docker exec -it'
-abbr dkh 'docker history'
-abbr dki 'docker images'
-abbr dkin 'docker inspect'
-abbr dkim 'docker import'
-abbr dkk 'docker kill'
-abbr dkkh 'docker kill -s HUP'
-abbr dkl 'docker logs'
-abbr dkL 'docker logs -f'
-abbr dkli 'docker login'
-abbr dklo 'docker logout'
-abbr dkls 'docker ps'
-abbr dkp 'docker pause'
-abbr dkP 'docker unpause'
-abbr dkpl 'docker pull'
-abbr dkph 'docker push'
-abbr dkps 'docker ps'
-abbr dkpsa 'docker ps -a'
-abbr dkr 'docker run'
-abbr dkri 'docker run -it --rm'
-abbr dkrie 'docker run -it --rm --entrypoint /bin/bash'
-abbr dkRM 'docker system prune'
-abbr dkrm 'docker rm'
-abbr dkrmi 'docker rmi'
-abbr dkrn 'docker rename'
-abbr dks 'docker start'
-abbr dkS 'docker restart'
-abbr dkss 'docker stats'
-abbr dksv 'docker save'
-abbr dkt 'docker tag'
-abbr dktop 'docker top'
-abbr dkup 'docker update'
-abbr dkV 'docker volume'
-abbr dkv 'docker version'
-abbr dkw 'docker wait'
-abbr dkx 'docker stop'
-abbr dkstop 'docker stop (docker ps -aq)'
+set -q __docker_abbr_initialized; and exit 0
 
-# Docker Compose (c)
-abbr dkc 'docker-compose'
-abbr dkcb 'docker-compose build'
-abbr dkcB 'docker-compose build --no-cache'
-abbr dkcd 'docker-compose down'
-abbr dkce 'docker-compose exec'
-abbr dkck 'docker-compose kill'
-abbr dkcl 'docker-compose logs'
-abbr dkcL 'docker-compose logs -f'
-abbr dkcls 'docker-compose ps'
-abbr dkcp 'docker-compose pause'
-abbr dkcP 'docker-compose unpause'
-abbr dkcpl 'docker-compose pull'
-abbr dkcph 'docker-compose push'
-abbr dkcps 'docker-compose ps'
-abbr dkcr 'docker-compose run'
-abbr dkcR 'docker-compose run --rm'
-abbr dkcrm 'docker-compose rm'
-abbr dkcs 'docker-compose start'
-abbr dkcsc 'docker-compose scale'
-abbr dkcS 'docker-compose restart'
-abbr dkcu 'docker-compose up'
-abbr dkcU 'docker-compose up -d'
-abbr dkcv 'docker-compose version'
-abbr dkcx 'docker-compose stop'
-#
-## Container (C)
-abbr dkC 'docker container'
-abbr dkCa 'docker container attach'
-abbr dkCcp 'docker container cp'
-abbr dkCd 'docker container diff'
-abbr dkCe 'docker container exec'
-abbr dkCei 'docker container exec -it'
-abbr dkCin 'docker container inspect'
-abbr dkCk 'docker container kill'
-abbr dkCl 'docker container logs'
-abbr dkCL 'docker container logs -f'
-abbr dkCls 'docker container ls'
-abbr dkCp 'docker container pause'
-abbr dkCpr 'docker container prune'
-abbr dkCrn 'docker container rename'
-abbr dkCS 'docker container restart'
-abbr dkCrm 'docker container rm'
-abbr dkCr 'docker container run'
-abbr dkCri 'docker container run -it --rm'
-abbr dkCrie 'docker container run -it --rm --entrypoint /bin/bash'
-abbr dkCs 'docker container start'
-abbr dkCss 'docker container stats'
-abbr dkCx 'docker container stop'
-abbr dkCtop 'docker container top'
-abbr dkCP 'docker container unpause'
-abbr dkCup 'docker container update'
-abbr dkCw 'docker container wait'
+set -U __docker_abbr_abbreviations
 
-## Image (I)
-abbr dkI 'docker image'
-abbr dkIb 'docker image build'
-abbr dkIh 'docker image history'
-abbr dkIim 'docker image import'
-abbr dkIin 'docker image inspect'
-abbr dkIls 'docker image ls'
-abbr dkIpr 'docker image prune'
-abbr dkIpl 'docker image pull'
-abbr dkIph 'docker image push'
-abbr dkIrm 'docker image rm'
-abbr dkIsv 'docker image save'
-abbr dkIt 'docker image tag'
+function __docker_abbr_install --on-event fish_docker_abbr_install
+    function __docker_abbr -d "Create docker abbreviations"
+        set -l name $argv[1]
+        set -l body $argv[2..-1]
+        abbr -a $name $body
+        set -a __docker_abbr_abbreviations $name
+    end
 
-## Volume (V)
-abbr dkV 'docker volume'
-abbr dkVin 'docker volume inspect'
-abbr dkVls 'docker volume ls'
-abbr dkVpr 'docker volume prune'
-abbr dkVrm 'docker volume rm'
+    # Docker
+    __docker_abbr dk 'docker'
+    __docker_abbr dka 'docker attach'
+    __docker_abbr dkb 'docker build'
+    __docker_abbr dkd 'docker diff'
+    __docker_abbr dkdf 'docker system df'
+    __docker_abbr dke 'docker exec'
+    __docker_abbr dkei 'docker exec -it'
+    __docker_abbr dkh 'docker history'
+    __docker_abbr dki 'docker images'
+    __docker_abbr dkin 'docker inspect'
+    __docker_abbr dkim 'docker import'
+    __docker_abbr dkk 'docker kill'
+    __docker_abbr dkkh 'docker kill -s HUP'
+    __docker_abbr dkl 'docker logs'
+    __docker_abbr dkL 'docker logs -f'
+    __docker_abbr dkli 'docker login'
+    __docker_abbr dklo 'docker logout'
+    __docker_abbr dkls 'docker ps'
+    __docker_abbr dkp 'docker pause'
+    __docker_abbr dkP 'docker unpause'
+    __docker_abbr dkpl 'docker pull'
+    __docker_abbr dkph 'docker push'
+    __docker_abbr dkps 'docker ps'
+    __docker_abbr dkpsa 'docker ps -a'
+    __docker_abbr dkr 'docker run'
+    __docker_abbr dkri 'docker run -it --rm'
+    __docker_abbr dkrie 'docker run -it --rm --entrypoint /bin/bash'
+    __docker_abbr dkRM 'docker system prune'
+    __docker_abbr dkrm 'docker rm'
+    __docker_abbr dkrmi 'docker rmi'
+    __docker_abbr dkrn 'docker rename'
+    __docker_abbr dks 'docker start'
+    __docker_abbr dkS 'docker restart'
+    __docker_abbr dkss 'docker stats'
+    __docker_abbr dksv 'docker save'
+    __docker_abbr dkt 'docker tag'
+    __docker_abbr dktop 'docker top'
+    __docker_abbr dkup 'docker update'
+    __docker_abbr dkv 'docker version'
+    __docker_abbr dkw 'docker wait'
+    __docker_abbr dkx 'docker stop'
+    __docker_abbr dkstop 'docker stop (docker ps -aq)'
 
-## Network (N)
-abbr dkN 'docker network'
-abbr dkNs 'docker network connect'
-abbr dkNx 'docker network disconnect'
-abbr dkNin 'docker network inspect'
-abbr dkNls 'docker network ls'
-abbr dkNpr 'docker network prune'
-abbr dkNrm 'docker network rm'
+    # Docker Compose (c)
+    __docker_abbr dkc 'docker-compose'
+    __docker_abbr dkcb 'docker-compose build'
+    __docker_abbr dkcB 'docker-compose build --no-cache'
+    __docker_abbr dkcd 'docker-compose down'
+    __docker_abbr dkce 'docker-compose exec'
+    __docker_abbr dkck 'docker-compose kill'
+    __docker_abbr dkcl 'docker-compose logs'
+    __docker_abbr dkcL 'docker-compose logs -f'
+    __docker_abbr dkcls 'docker-compose ps'
+    __docker_abbr dkcp 'docker-compose pause'
+    __docker_abbr dkcP 'docker-compose unpause'
+    __docker_abbr dkcpl 'docker-compose pull'
+    __docker_abbr dkcph 'docker-compose push'
+    __docker_abbr dkcps 'docker-compose ps'
+    __docker_abbr dkcr 'docker-compose run'
+    __docker_abbr dkcR 'docker-compose run --rm'
+    __docker_abbr dkcrm 'docker-compose rm'
+    __docker_abbr dkcs 'docker-compose start'
+    __docker_abbr dkcsc 'docker-compose scale'
+    __docker_abbr dkcS 'docker-compose restart'
+    __docker_abbr dkcu 'docker-compose up'
+    __docker_abbr dkcU 'docker-compose up -d'
+    __docker_abbr dkcv 'docker-compose version'
+    __docker_abbr dkcx 'docker-compose stop'
+    #
+    ## Container (C)
+    __docker_abbr dkC 'docker container'
+    __docker_abbr dkCa 'docker container attach'
+    __docker_abbr dkCcp 'docker container cp'
+    __docker_abbr dkCd 'docker container diff'
+    __docker_abbr dkCe 'docker container exec'
+    __docker_abbr dkCei 'docker container exec -it'
+    __docker_abbr dkCin 'docker container inspect'
+    __docker_abbr dkCk 'docker container kill'
+    __docker_abbr dkCl 'docker container logs'
+    __docker_abbr dkCL 'docker container logs -f'
+    __docker_abbr dkCls 'docker container ls'
+    __docker_abbr dkCp 'docker container pause'
+    __docker_abbr dkCpr 'docker container prune'
+    __docker_abbr dkCrn 'docker container rename'
+    __docker_abbr dkCS 'docker container restart'
+    __docker_abbr dkCrm 'docker container rm'
+    __docker_abbr dkCr 'docker container run'
+    __docker_abbr dkCri 'docker container run -it --rm'
+    __docker_abbr dkCrie 'docker container run -it --rm --entrypoint /bin/bash'
+    __docker_abbr dkCs 'docker container start'
+    __docker_abbr dkCss 'docker container stats'
+    __docker_abbr dkCx 'docker container stop'
+    __docker_abbr dkCtop 'docker container top'
+    __docker_abbr dkCP 'docker container unpause'
+    __docker_abbr dkCup 'docker container update'
+    __docker_abbr dkCw 'docker container wait'
 
-## System (Y)
-abbr dkY 'docker system'
-abbr dkYdf 'docker system df'
-abbr dkYpr 'docker system prune'
+    ## Image (I)
+    __docker_abbr dkI 'docker image'
+    __docker_abbr dkIb 'docker image build'
+    __docker_abbr dkIh 'docker image history'
+    __docker_abbr dkIim 'docker image import'
+    __docker_abbr dkIin 'docker image inspect'
+    __docker_abbr dkIls 'docker image ls'
+    __docker_abbr dkIpr 'docker image prune'
+    __docker_abbr dkIpl 'docker image pull'
+    __docker_abbr dkIph 'docker image push'
+    __docker_abbr dkIrm 'docker image rm'
+    __docker_abbr dkIsv 'docker image save'
+    __docker_abbr dkIt 'docker image tag'
 
-## Stack (K)
-abbr dkK 'docker stack'
-abbr dkKls 'docker stack ls'
-abbr dkKps 'docker stack ps'
-abbr dkKrm 'docker stack rm'
+    ## Volume (V)
+    __docker_abbr dkV 'docker volume'
+    __docker_abbr dkVin 'docker volume inspect'
+    __docker_abbr dkVls 'docker volume ls'
+    __docker_abbr dkVpr 'docker volume prune'
+    __docker_abbr dkVrm 'docker volume rm'
 
-## Swarm (W)
-abbr dkW 'docker swarm'
+    ## Network (N)
+    __docker_abbr dkN 'docker network'
+    __docker_abbr dkNs 'docker network connect'
+    __docker_abbr dkNx 'docker network disconnect'
+    __docker_abbr dkNin 'docker network inspect'
+    __docker_abbr dkNls 'docker network ls'
+    __docker_abbr dkNpr 'docker network prune'
+    __docker_abbr dkNrm 'docker network rm'
 
-## CleanUp (rm)
-# Clean up exited containers (docker < 1.13)
-abbr dkrmC 'docker rm (docker ps -qaf status=exited)'
+    ## System (Y)
+    __docker_abbr dkY 'docker system'
+    __docker_abbr dkYdf 'docker system df'
+    __docker_abbr dkYpr 'docker system prune'
 
-# Clean up dangling images (docker < 1.13)
-abbr dkrmI 'docker rmi (docker images -qf dangling=true)'
+    ## Stack (K)
+    __docker_abbr dkK 'docker stack'
+    __docker_abbr dkKls 'docker stack ls'
+    __docker_abbr dkKps 'docker stack ps'
+    __docker_abbr dkKrm 'docker stack rm'
 
-# Pull all tagged images
-abbr dkplI 'docker images --format "{{ .Repository }}" | grep -v "^<none>$" | xargs -L1 docker pull'
+    ## Swarm (W)
+    __docker_abbr dkW 'docker swarm'
 
-# Clean up dangling volumes (docker < 1.13)
-abbr dkrmV 'docker volume rm (docker volume ls -qf dangling=true)'
+    ## CleanUp (rm)
+    # Clean up exited containers (docker < 1.13)
+    __docker_abbr dkrmC 'docker rm (docker ps -qaf status=exited)'
 
-# Docker Machine (m)
-abbr dkm 'docker-machine'
-abbr dkma 'docker-machine active'
-abbr dkmcp 'docker-machine scp'
-abbr dkmin 'docker-machine inspect'
-abbr dkmip 'docker-machine ip'
-abbr dkmk 'docker-machine kill'
-abbr dkmls 'docker-machine ls'
-abbr dkmpr 'docker-machine provision'
-abbr dkmps 'docker-machine ps'
-abbr dkmrg 'docker-machine regenerate-certs'
-abbr dkmrm 'docker-machine rm'
-abbr dkms 'docker-machine start'
-abbr dkmsh 'docker-machine ssh'
-abbr dkmst 'docker-machine status'
-abbr dkmS 'docker-machine restart'
-abbr dkmu 'docker-machine url'
-abbr dkmup 'docker-machine upgrade'
-abbr dkmv 'docker-machine version'
-abbr dkmx 'docker-machine stop'
+    # Clean up dangling images (docker < 1.13)
+    __docker_abbr dkrmI 'docker rmi (docker images -qf dangling=true)'
+
+    # Pull all tagged images
+    __docker_abbr dkplI 'docker images --format "{{ .Repository }}" | grep -v "^<none>$" | xargs -L1 docker pull'
+
+    # Clean up dangling volumes (docker < 1.13)
+    __docker_abbr dkrmV 'docker volume rm (docker volume ls -qf dangling=true)'
+
+    # Docker Machine (m)
+    __docker_abbr dkm 'docker-machine'
+    __docker_abbr dkma 'docker-machine active'
+    __docker_abbr dkmcp 'docker-machine scp'
+    __docker_abbr dkmin 'docker-machine inspect'
+    __docker_abbr dkmip 'docker-machine ip'
+    __docker_abbr dkmk 'docker-machine kill'
+    __docker_abbr dkmls 'docker-machine ls'
+    __docker_abbr dkmpr 'docker-machine provision'
+    __docker_abbr dkmps 'docker-machine ps'
+    __docker_abbr dkmrg 'docker-machine regenerate-certs'
+    __docker_abbr dkmrm 'docker-machine rm'
+    __docker_abbr dkms 'docker-machine start'
+    __docker_abbr dkmsh 'docker-machine ssh'
+    __docker_abbr dkmst 'docker-machine status'
+    __docker_abbr dkmS 'docker-machine restart'
+    __docker_abbr dkmu 'docker-machine url'
+    __docker_abbr dkmup 'docker-machine upgrade'
+    __docker_abbr dkmv 'docker-machine version'
+    __docker_abbr dkmx 'docker-machine stop'
+
+    functions --erase __docker_abbr
+end
+
+function __docker_abbr_update --on-event fish_docker_abbr_update
+    __git_abbr_uninstall
+    __git_abbr_install
+end
 
 function __fish_docker_abbr_uninstall --on-event fish_docker_abbr_uninstall
-    abbr --erase dk
-    abbr --erase dka
-    abbr --erase dkb
-    abbr --erase dkd
-    abbr --erase dkdf
-    abbr --erase dke
-    abbr --erase dkei
-    abbr --erase dkh
-    abbr --erase dki
-    abbr --erase dkin
-    abbr --erase dkim
-    abbr --erase dkk
-    abbr --erase dkkh
-    abbr --erase dkl
-    abbr --erase dkL
-    abbr --erase dkli
-    abbr --erase dklo
-    abbr --erase dkls
-    abbr --erase dkp
-    abbr --erase dkP
-    abbr --erase dkpl
-    abbr --erase dkph
-    abbr --erase dkps
-    abbr --erase dkpsa
-    abbr --erase dkr
-    abbr --erase dkri
-    abbr --erase dkrie
-    abbr --erase dkRM
-    abbr --erase dkrm
-    abbr --erase dkrmi
-    abbr --erase dkrn
-    abbr --erase dks
-    abbr --erase dkS
-    abbr --erase dkss
-    abbr --erase dksv
-    abbr --erase dkt
-    abbr --erase dktop
-    abbr --erase dkup
-    abbr --erase dkV
-    abbr --erase dkv
-    abbr --erase dkw
-    abbr --erase dkx
-    abbr --erase dkc
-    abbr --erase dkcb
-    abbr --erase dkcB
-    abbr --erase dkcd
-    abbr --erase dkce
-    abbr --erase dkck
-    abbr --erase dkcl
-    abbr --erase dkcL
-    abbr --erase dkcls
-    abbr --erase dkcp
-    abbr --erase dkcP
-    abbr --erase dkcpl
-    abbr --erase dkcph
-    abbr --erase dkcps
-    abbr --erase dkcr
-    abbr --erase dkcR
-    abbr --erase dkcrm
-    abbr --erase dkcs
-    abbr --erase dkcsc
-    abbr --erase dkcS
-    abbr --erase dkcu
-    abbr --erase dkcU
-    abbr --erase dkcv
-    abbr --erase dkcx
-    abbr --erase dkC
-    abbr --erase dkCa
-    abbr --erase dkCcp
-    abbr --erase dkCd
-    abbr --erase dkCe
-    abbr --erase dkCei
-    abbr --erase dkCin
-    abbr --erase dkCk
-    abbr --erase dkCl
-    abbr --erase dkCL
-    abbr --erase dkCls
-    abbr --erase dkCp
-    abbr --erase dkCpr
-    abbr --erase dkCrn
-    abbr --erase dkCS
-    abbr --erase dkCrm
-    abbr --erase dkCr
-    abbr --erase dkCri
-    abbr --erase dkCrie
-    abbr --erase dkCs
-    abbr --erase dkCss
-    abbr --erase dkCx
-    abbr --erase dkCtop
-    abbr --erase dkCP
-    abbr --erase dkCup
-    abbr --erase dkCw
-    abbr --erase dkI
-    abbr --erase dkIb
-    abbr --erase dkIh
-    abbr --erase dkIim
-    abbr --erase dkIin
-    abbr --erase dkIls
-    abbr --erase dkIpr
-    abbr --erase dkIpl
-    abbr --erase dkIph
-    abbr --erase dkIrm
-    abbr --erase dkIsv
-    abbr --erase dkIt
-    abbr --erase dkVin
-    abbr --erase dkVls
-    abbr --erase dkVpr
-    abbr --erase dkVrm
-    abbr --erase dkN
-    abbr --erase dkNs
-    abbr --erase dkNx
-    abbr --erase dkNin
-    abbr --erase dkNls
-    abbr --erase dkNpr
-    abbr --erase dkNrm
-    abbr --erase dkY
-    abbr --erase dkYdf
-    abbr --erase dkYpr
-    abbr --erase dkK
-    abbr --erase dkKls
-    abbr --erase dkKps
-    abbr --erase dkKrm
-    abbr --erase dkW
-    abbr --erase dkrmC
-    abbr --erase dkrmI
-    abbr --erase dkplI
-    abbr --erase dkrmV
-    abbr --erase dkm
-    abbr --erase dkma
-    abbr --erase dkmcp
-    abbr --erase dkmin
-    abbr --erase dkmip
-    abbr --erase dkmk
-    abbr --erase dkmls
-    abbr --erase dkmpr
-    abbr --erase dkmps
-    abbr --erase dkmrg
-    abbr --erase dkmrm
-    abbr --erase dkms
-    abbr --erase dkmsh
-    abbr --erase dkmst
-    abbr --erase dkmS
-    abbr --erase dkmu
-    abbr --erase dkmup
-    abbr --erase dkmv
-    abbr --erase dkmx
-    abbr --erase dkstop
+    for ab in $__docker_abbr_abbreviations
+        abbr --erase $ab
+    end
+    set -Ue __docker_abbr_abbreviations
+    set -Ue __docker_abbr_initialized
 end
+
+# Mark docker_abbr plugin as initialized
+set -U __docker_abbr_initialized (date)
